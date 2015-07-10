821f8304a56294f68647e134e8b0a960dbcd0e2d
connor zhang <dreamkeep550@gmail.com>
2015-07-10 21:48:54 +0800

    add func:top, para:G_current_messages for Gokit

--- product/linux/main.c
+++ product/linux/main.c
@@ -20,11 +20,22 @@
 #include <netinet/in.h>
 #include "x86_drv.h"
 
+#include <common/mavlink.h>
+
 #define GAGENT_TIMER_S_1        1
 #define GAGENT_TIMER_US         0
+
+// declare top ,it come from mavlink_control.so
+extern int top(int argc, char **argv);
+extern  Mavlink_Messages G_current_messages;
+
+
 int main(void)
 {
     /* 创建gagent进程单独运行 */
+	int argc,;
+	char **argv;
+	
     //CoreInit();
     IOF_Config_hook_init();
 	IOF_General_Hook_Init();
@@ -32,7 +43,7 @@ int main(void)
     X86_Timer_Creat(GAGENT_TIMER_S_1, GAGENT_TIMER_US, x86_timer_handle);       /* 1S */
 	
     GAgent_Init();
-
+	top(argc,argv);
     while(1)
     {
         GAgent_TimerRun();
